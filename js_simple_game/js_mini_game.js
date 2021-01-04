98ca1379b71fec98d679474a8d29fc4062147fa7
Piyumi Dulanjali <65762392+Piyumi047@users.noreply.github.com>
2021-01-05 00:58:36 +0530

    js_simple_game

---
+++ js_simple_game/js_mini_game.js
@@ -0,0 +1,54 @@
+var start= new Date().getTime();
+      
+       function getRandomColor(){
+          var letters = '0123456789ABCDEF';
+          var color = '#';
+          for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+          }
+          return color;
+        }
+
+
+
+
+       function shapeAppear(){
+           var top=Math.random()*550;
+           var left=Math.random()*550;
+           var width=Math.random()*150;
+           var height=Math.random()*150;
+
+           if(Math.random()>0.5){
+              document.getElementById("shape").style.borderRadius="50%"
+           }
+           else{
+              document.getElementById("shape").style.borderRadius="0%"
+           }
+          
+           document.getElementById("shape").style.top=top+"px";
+           document.getElementById("shape").style.left=left+"px";
+           document.getElementById("shape").style.width=width+"px";
+           document.getElementById("shape").style.height=width+"px";
+           document.getElementById("shape").style.display="block";
+           document.getElementById("shape").style.backgroundColor=getRandomColor();
+           start= new Date().getTime();
+       }
+
+
+
+       function shapeAppeardelay(){
+       setTimeout(shapeAppear, Math.random()*2500);
+       }
+
+
+       shapeAppeardelay();
+
+
+
+       document.getElementById("shape").onclick=function(){
+            document.getElementById("shape").style.display="none";
+            var end= new Date().getTime();
+            var time= (end -start)/1000;
+            document.getElementById("time").innerHTML=time+"s";
+            shapeAppeardelay();
+       }
